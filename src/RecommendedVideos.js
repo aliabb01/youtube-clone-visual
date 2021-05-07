7998f1415f9763754e80a8045ff7a7080d71ad53
aliabb01 <aliabbasov21112001@gmail.com>
2021-05-07 14:40:19 +0400

    RecommendedVideos component

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import VideoCard from './VideoCard';
+import './RecommendedVideos.css'
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+                <VideoCard
+                    title = "Become a web developer in 10 minutes | 2020/2021"
+                    views = "2.3M Views"
+                    timestamp = "3 days ago"
+                    channelImage = ""
+                    channel = "Ali Abbasov"
+                    image = "https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RecommendedVideos
