b96dd43445e13fcb9f88f0f3bd58086d843192e8
aliabb <ali3abbasov@gmail.com>
2021-07-26 13:22:22 +0400

    refactor: SearchPage component
    Add different videos instead of one

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -25,49 +25,50 @@ function SearchPage() {
             <hr />
 
             <VideoRow
-            views="399"
-            subs="1.1K"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime dolores temporibus similique perferendis aut rem possimus vel quidem iure?"
-            timestamp="59 seconds ago"
-            channel="Ali Abbasov"
-            title="Learning Web Development"
-            image="https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg" />
+                views="109K"
+                subs="201K"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime dolores temporibus similique perferendis aut rem possimus vel quidem iure?"
+                timestamp="1 week ago"
+                channel="John Smith"
+                title="React Explained for Vue Developers"
+                image="https://i.ytimg.com/vi/oipl-RXyLTw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDQ5Gym-4Qvy9jb0awhZVeYRIa6tA" 
+            />
 
             <VideoRow
             views="399"
             subs="1.1K"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime dolores temporibus similique perferendis aut rem possimus vel quidem iure?"
             timestamp="59 seconds ago"
-            channel="Ali Abbasov"
-            title="Learning Web Development"
-            image="https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg" />
+            channel="John Smith"
+            title="Learn JavaScript Array Reduce in 10 Minutes"
+            image="https://i.ytimg.com/vi/s1XVfm5mIuU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBn76N7faH4nz-3XVdANW0Qe0Nw4g" />
 
             <VideoRow
-            views="399"
-            subs="1.1K"
+            views="15K"
+            subs="10K"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime dolores temporibus similique perferendis aut rem possimus vel quidem iure?"
-            timestamp="59 seconds ago"
-            channel="Ali Abbasov"
-            title="Learning Web Development"
-            image="https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg" />
+            timestamp="2 weeks ago"
+            channel="John Smith"
+            title="Career Advice For Android Developers // App Developer Career Paths"
+            image="https://i.ytimg.com/vi/zyR2qVn4X6A/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCwQP3f5mSrjSFQxbey3TCZVGimeA" />
 
             <VideoRow
-            views="399"
-            subs="1.1K"
+            views="1.3M"
+            subs="2.8M"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime dolores temporibus similique perferendis aut rem possimus vel quidem iure?"
-            timestamp="59 seconds ago"
-            channel="Ali Abbasov"
-            title="Learning Web Development"
-            image="https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg" />
+            timestamp="5 days ago"
+            channel="RealLifeLore"
+            title="Why 50% of Canadians Live South of This Line"
+            image="https://i.ytimg.com/vi/DFJAgb7dn78/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAnxup-56275Eqw88SYITymLrYLyw" />
 
             <VideoRow
-            views="399"
+            views="1.9K Views"
             subs="1.1K"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime dolores temporibus similique perferendis aut rem possimus vel quidem iure?"
-            timestamp="59 seconds ago"
-            channel="Ali Abbasov"
-            title="Learning Web Development"
-            image="https://i.ytimg.com/vi/NT299zIk2JY/mqdefault.jpg" />
+            timestamp="4 months ago"
+            channel="John Smith"
+            title="React Native vs Flutter. Which Framework to chose?"
+            image="https://i.ytimg.com/vi/VU8Yv6LhDlY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDoB8lRkfHuL4eSKME7fSwKxpbx-g" />
         </div>
     )
 }
