c587acce931d3873e2dc59a0fdf26b1a3e106a4c
aliabb <ali3abbasov@gmail.com>
2021-07-26 13:17:24 +0400

    refactor: Update VideoCard component
    Add verified icon, video length and correct height and width

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import './VideoCard.css';
 
-function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
+function VideoCard({ image, title, channel, views, timestamp, channelImage, verified, length }) {
     return (
         <div className="videoCard">
-            <img className="videoCard__thumbnail" src={image} alt="" />
+            <div style={{ position: 'relative' }}>
+                <img className="videoCard__thumbnail" src={image} alt="" />
+                <div style={{ position: 'absolute', bottom: '10px', right: '20px', zIndex: '3', background: 'black', color: 'white', padding: 2, paddingTop: 1, paddingBottom: 1, borderRadius: '2px' }}>
+                    <p style={{ fontSize: '0.8rem' }}>{ length }</p>
+                </div>
+            </div>
             <div className="videoCard__info">
-                <Avatar className="videoCard__avatar" alt={channel} src={channel} />
+                <Avatar className="videoCard__avatar" alt={channel} src={channelImage ? channelImage : channel} />
                 <div className="videoCard__text">
-                    <h4>{title}</h4>
-                    <p>{channel}</p>
+                    <p style={{ color: 'black', fontSize: '1.05rem', fontWeight: '500' }}>{title}</p>
+                    <p>{channel} {verified && <CheckCircleIcon style={{ fontSize: '1rem' }} />} </p>
                     <p>{views} • {timestamp}</p>
                 </div>
             </div>
